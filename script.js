8698f6f39212860c296f5ed0c5d8771a9568d4e4
Xileno <gilenoalyss@gmail.com>
2025-03-07 01:31:28 -0300

    parte 2.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function somaDigitos(N){
      let primeiraIteracao = calcularSoma(n);
      console.log(`Primeira interação: ${primeiraIteracao}`);
      
+     if (primeiraIteracao >= 10) {
+        let segundaIteracao = calcularSoma(primeiraIteracao);
+        console.log(`Segunda iteração: ${segundaIteracao}`);
+    } else {
+        console.log("Segunda iteração: Não é possível calcular.");
+    }
+}
