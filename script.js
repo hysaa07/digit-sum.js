2f38bd9624c140b036a82b884576e3020f35e39b
root <root@vbox.ifrn.local>
2025-03-11 21:28:26 -0300

    função somaDigitos

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-
-codigo
+function somaDigitos(N){
+    function
+    soma_digitos_uma_vez(numero){
+        let soma = 0;
+        while (numero > 0){
+            soma += numero % 10;
+            numero =
+            Math.floor(numero / 10);
+        }
+                return soma
+            }
+            //Primeira aplicação
+            let resultado = 
+            soma_digitos_uma_vez(N)
+            console.log("Primeira interação:", resultado);
+}
+    
