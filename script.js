260b18cd2a964d01861cf17cabc636d5b41120a4
root <root@vbox.ifrn.local>
2025-03-11 21:53:19 -0300

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 function somaDigitos(N){
-    function
-    soma_digitos_uma_vez(numero){
-        let soma = 0;
-        while (numero > 0){
-            soma += numero % 10;
-            numero =
-            Math.floor(numero / 10);
-        }
-                return soma
-            }
-            //Primeira aplicação
-            let resultado = 
-            soma_digitos_uma_vez(N)
-            console.log("Primeira interação:", resultado);
-}
-    
+    function calcularSoma (soma){
+        return numtoString().split('').reduce((Acc, digit) => acc + perseInt(digit), 0);
+    }
+     let primeiraIteracao = calcularSoma(n);
+     console.log(`Primeira interação: ${primeiraIteracao}`);
+     
